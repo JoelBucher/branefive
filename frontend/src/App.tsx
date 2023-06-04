78ce4cf894475af42a87d9b66f77ed08e70adf74
Joel Bucher <mail@joelbucher.ch>
2023-06-04 23:20:51 +0200

    build: added mantine header

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import { HeaderResponsive } from './components/header';
+function App() {
+  return (
+    <HeaderResponsive/>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useState } from 'react';
+import data from '../data/data.json';
+import {
+  Header,
+  Container,
+  Group,
+  Burger,
+  Paper,
+  Transition,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { MantineLogo } from '@mantine/ds';
+import { HEADER_HEIGHT } from '../utils/constants';
+import { useHeaderStyles } from '../hooks/useHeaderStyles';
+
+interface HeaderResponsiveProps {
+  links: { link: string; label: string }[];
+}
+
+export function HeaderResponsive() {
+  const links = data.header;
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useHeaderStyles();
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+        close();
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
+      <Container className={classes.header}>
+        <MantineLogo size={28} />
+        <Group spacing={5} className={classes.links}>
+          {items}
+        </Group>
+
+        <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+              {items}
+            </Paper>
+          )}
+        </Transition>
+      </Container>
+    </Header>
+  );
+}
