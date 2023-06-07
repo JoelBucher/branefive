6cbd1976ef5b5964f0724244f77c669fb71fb441
Joel Bucher <mail@joelbucher.ch>
2023-06-08 00:32:07 +0200

    newsletter section done

--- frontend/src/components/NewsletterComponent.tsx
+++ frontend/src/components/NewsletterComponent.tsx
@@ -1,13 +1,26 @@
-import { Container, Text, Button, Group } from '@mantine/core';
+import { Container, Text, Button, Group, Image, Grid, Avatar, MediaQuery } from '@mantine/core';
 import { GithubIcon } from '@mantine/ds';
 import { useNewsletterStyle } from '../hooks/useNewsletterStyle';
+import enjoyerOneSrc from '../assets/enjoyer1.jpg';
+import enjoyerTwoSrc from '../assets/enjoyer2.png';
 
-export function Newsletter() {
+function NewsLetterImage(){
+  return(
+    <>
+      <Avatar.Group spacing="20%">
+        <Avatar src={enjoyerTwoSrc} size="70%" radius="100%"/>
+        <Avatar src={enjoyerOneSrc} size="70%" radius="100%"/>
+      </Avatar.Group>
+    </>
+  )
+}
+
+
+function NewsletterText(){
   const { classes } = useNewsletterStyle();
 
-  return (
-    <div className={classes.wrapper}>
-      <Container size={700} className={classes.inner}>
+  return(
+    <>
         <h1 className={classes.title}>
           Join the Branefive{' '}
           <Text component="span" variant="gradient" gradient={{ from: 'red', to: 'magenta' }} inherit>
@@ -16,7 +29,7 @@ export function Newsletter() {
         </h1>
 
         <Text className={classes.description} color="dimmed">
-          Get updates about new Song releases and upcoming tours.
+          Get updates about new Song releases and upcoming gigs.
         </Text>
 
         <Group className={classes.controls}>
@@ -29,7 +42,26 @@ export function Newsletter() {
             Join now!
           </Button>
         </Group>
-      </Container>
-    </div>
+        </>
+  )
+}
+
+export function Newsletter() {
+  const { classes } = useNewsletterStyle();
+  return (
+    <Grid align='center' justify='center'>
+      <Grid.Col sm={5} lg={4}>
+        <NewsletterText/>
+      </Grid.Col>
+      <Grid.Col sm={5} lg={4}>
+        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+          <Avatar.Group spacing="20%">
+            <Avatar src={enjoyerTwoSrc} size="70%" radius="100%"/>
+            <Avatar src={enjoyerOneSrc} size="70%" radius="100%"/>
+          </Avatar.Group>
+        </MediaQuery>
+      </Grid.Col>
+      
+    </Grid>
   );
 }
