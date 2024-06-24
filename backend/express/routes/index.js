f2b9757a2852e299e332f529652375481cac110a
Joel Bucher <mail@joelbucher.ch>
2024-06-24 09:30:38 +0200

    .

--- backend/express/routes/index.js
+++ backend/express/routes/index.js
@@ -1,31 +1,15 @@
 var express = require('express');
-var pgp = require('pg-promise');
+const db = require('./db');
 
 var router = express.Router();
 
-const user = process.env.POSTGRES_USER;
-const password = process.env.POSTGRES_PASSWORD;
-const host = process.env.POSTGRES_HOST;
-const port = process.env.POSTGRES_PORT;
-const database = process.env.POSTGRES_DATABASE;
-const access= `postgres://${user}:${password}@${host}:${port}`;
-// const db = pgp(access)
-
 router.get('/', function(req, res, next) {
-  res.send(access);
+  res.send("hello world");
 });
 
-/*
 router.get('/postgres', function(req, res, next) {
-  db.one('SELECT $1 AS value', 123)
-  .then((data) => {
-    res.send(data.value)
-  })
-  .catch((error) => {
-    console.log('ERROR:', error)
-  })
+  
 });
-*/
 
 
 module.exports = router;
